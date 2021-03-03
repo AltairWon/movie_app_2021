fb790dcf66d10095a7386f22e6e9942f796ed975
whj121 <whj121@estorm.co.kr>
2021-03-03 16:57:54 +0900

    #1.2 How does React work?

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root') //react 는 App.js 에다가 존재를 하게 하기 위해서는 ElementByld내부에 넣으려고 한다.
+  //virtual DOM(virtual document object model) = 존재하지 않는다. 소스코드에는 존재하지 않지만 react에서 만들어진다.
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
