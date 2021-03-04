93d2e7453c0295aa10c6f8d1ab893aa07ce1dd52
whj121 <whj121@estorm.co.kr>
2021-03-04 12:21:43 +0900

    #2.4 Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import PropTypes from "prop-types"
 
 const foodILike = [
   {
     id:1,
     name: "Kimchi",
     image:
-      "https://www.maangchi.com/wp-content/uploads/2014/06/whole-cabbage-kimchi.jpg"
+      "https://www.maangchi.com/wp-content/uploads/2014/06/whole-cabbage-kimchi.jpg",
+      rating: 5
   },
   {
     id:2,
     name: "Samgyeopsal",
     image:
-      "https://i0.wp.com/seonkyounglongest.com/wp-content/uploads/2019/06/samgyeopsal-20.jpg?fit=1000%2C1495&ssl=1"
+      "https://i0.wp.com/seonkyounglongest.com/wp-content/uploads/2019/06/samgyeopsal-20.jpg?fit=1000%2C1495&ssl=1",
+      rating: 4.3
   }
 ] //object list
 
@@ -27,13 +30,14 @@ function App() {
     {foodILike.map(renderFood)}
   </div>
 */
-function Food({name, picture}) { 
+function Food({name, picture, rating}) { 
   //react magic
   //props object 내부에는 fav가 있다. 내부에서 얻는 방법 props.fav
   //props는 argument로 간다. 
 
   return <div>
     <h2>I like {name}</h2>
+    <h4>{rating}/5.0</h4>
     <img src={picture} alt={name} />
   </div>; 
   //이렇게 해도 component에다가 정보를 보낼 수 있다.
@@ -42,15 +46,32 @@ function Food({name, picture}) {
   //alt={name} 이거은 img 요소들은 alt prop가 있어야 하기에 사용하는 것
 }
 
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
+//propTypes는 console에서 어떠한 오류가 범해져 있는지를 파악하는 방법이다.
+//이거는 무조건 propTypes로 설정을 해줘야 한다.
+
 function App() {
   return (
     
   <div>
-    {foodILike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image} />)}
+    {foodILike.map(dish => (
+    <Food 
+      key={dish.id} 
+      name={dish.name} 
+      picture={dish.image} 
+      rating={dish.rating} 
+    />
+    ))}
   </div>
+  );
+}
+
   
-  /*
-  <h1>Hello World!!</h1>
+  /*<h1>Hello World!!</h1>
   HTML <div class="hello">비슷하다.
   food component에 fav라는 이름의 property를 kimchi라는 value로 prop name을 줬다.
   props라는 개념을 잘 기억하자
@@ -63,7 +84,6 @@ function App() {
   prop에다가 유니크한 key를 입력하기 위해서 id를 추가해준다
   */
 
-  );
-}
+  
 
 export default App;
