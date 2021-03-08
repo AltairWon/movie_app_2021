df45cfa62d1e421226f921cbb7ba401c8f901c5d
whj121 <whj121@estorm.co.kr>
2021-03-08 14:23:07 +0900

    #6.3 Sharing Props Between Routes

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navigation.css";
 
 function Navigation() {
     return (
@@ -7,7 +8,7 @@ function Navigation() {
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
         </div>
-    )
+    );
 }
 
 export default Navigation;
